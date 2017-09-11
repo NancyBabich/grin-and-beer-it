cbe9af508803eed92291b1502ade152ee855494c
Maciek <maciej.m.maslowski@gmail.com>
2017-09-11 21:41:27 +0200

    fix global style import

--- app/index.js
+++ app/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import globalStyle from './styles/globalStyle';
 
 ReactDOM.render(<App />, document.getElementById('app'));
