b344f82c7f4553bdbae4e34a1f2d7576f01adb03
Maciek <maciej.m.maslowski@gmail.com>
2017-09-19 09:33:26 +0200

    [wip] create burger menu

--- app/components/BeerList/Header.js
+++ app/components/BeerList/Header.js
@@ -4,23 +4,41 @@ import styled from 'styled-components';
 import Colors from '../../consts/colors';
 
 const Header = () => (
-  <H1>
-    <ColoredText>beer</ColoredText>guru
-  </H1>
+  <HeaderContainer>
+    <NameContainer>
+      <H1>
+        <ColoredText>beer</ColoredText>guru
+      </H1>
+    </NameContainer>
+    <BurgerMenu>BM</BurgerMenu>
+  </HeaderContainer>
 );
 
 export default Header;
+
+const BurgerMenu = styled.div`
+  display: inline-block;
+  float: right;
+`;
+
+const ColoredText = styled.span`color: ${Colors.themeColor};`;
 
 const H1 = styled.h1`
   font-family: 'Open Sans';
   font-size: 2.5rem;
   text-transform: uppercase;
   color: ${Colors.darkGray};
-  margin: 7rem 0 0 5rem;
+`;
 
+const HeaderContainer = styled.div`
+  width: 100%;
+  padding: 7rem 0 0 5rem;
+  
   @media screen and (max-width: 995px) {
-    margin: 7rem 0 0 0;
+    padding: 7rem 0 0 0;
   }
 `;
 
-const ColoredText = styled.span`color: ${Colors.themeColor};`;
+const NameContainer = styled.div`
+  display: inline-block;
+`;
