9b483367b6087f5e8550057abef86a5957a1d7e3
Maciek <maciej.m.maslowski@gmail.com>
2017-09-20 20:37:59 +0200

    [wip]

--- app/components/BeerList/SortIcon.js
+++ app/components/BeerList/SortIcon.js
@@ -6,16 +6,22 @@ import Colors from '../../consts/colors';
 
 const SortIcon = ({ onClick, sortingOrder }) => (
   <SortIconContainer onClick={onClick}>
+    <Icon>
     <IconBar size={sortingOrder === 'desc' ? 'big' : 'small'} />
     <IconBar size="medium" />
     <IconBar size={sortingOrder === 'desc' ? 'small' : 'big'} />
+    </Icon>
   </SortIconContainer>
 );
 
 export default SortIcon;
 
+const Icon = styled.div`
+    position: absolute;
+`;
+
 const IconBar = styled.div`
-  height: 2px;
+  height: 1px;
   width: ${switchProp('size', {
     small: '40%',
     medium: '60%',
@@ -25,11 +31,9 @@ const IconBar = styled.div`
 `;
 
 const SortIconContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: 25px;
-  height: 25px;
+  position: relative;
+  width: 30px;
+  height: 30px;
 
   &:hover {
     cursor: pointer;
--- app/helpers/sortBeers.js
+++ app/helpers/sortBeers.js
@@ -1,9 +1,9 @@
 const sortBeers = (beers, sortingCategory, sortingOrder) => {
   if (sortingCategory && sortingOrder) {
     return beers.slice().sort((a, b) => {
-      if (sortingOrder === 'asc')
-        return a[sortingCategory] < b[sortingCategory] ? -1 : 1;
-      else return a[sortingCategory] < b[sortingCategory] ? 1 : -1;
+    //   if (sortingOrder === 'asc')
+        return a[sortingCategory] - b[sortingCategory];
+      //else return a[sortingCategory] < b[sortingCategory] ? 1 : -1;
     });
   } else return beers;
 };
